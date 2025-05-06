c559a1d7dd3b70e8c1633c7017245e378585aed2
SpencerCGriffiths <130156132+SpencerCGriffiths@users.noreply.github.com>
2025-05-06 19:44:06 +0100

    VEI-143 create EuTaxReference and begin to fix testing

---
+++ test/controllers/euDetails/EuTaxReferenceControllerSpec.scala
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.euDetails
+
+import base.SpecBase
+import controllers.euDetails.routes
+import forms.euDetails.EuTaxReferenceFormProvider
+import models.{Country, Index, NormalMode, UserAnswers}
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.euDetails.EuTaxReferencePage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.{AuthenticatedUserAnswersRepository, SessionRepository}
+import views.html.euDetails.EuTaxReferenceView
+
+import scala.concurrent.Future
+
+class EuTaxReferenceControllerSpec extends SpecBase with MockitoSugar {
+  
+  private val country: Country = arbitraryCountry.arbitrary.sample.value
+  private val countryIndex: Index = Index(0)
+  
+  private val formProvider = new EuTaxReferenceFormProvider()
+  private val form = formProvider(country)
+
+  private lazy val euTaxReferenceRoute = routes.EuTaxReferenceController.onPageLoad(waypoints, countryIndex).url
+
+  "EuTaxReference Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, euTaxReferenceRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[EuTaxReferenceView]
+
+        status(result) `mustBe` OK
+        contentAsString(result) `mustBe` view(form, waypoints, countryIndex, country)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(EuTaxReferencePage(???), "answer").success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, euTaxReferenceRoute)
+
+        val view = application.injector.instanceOf[EuTaxReferenceView]
+
+        val result = route(application, request).value
+
+        status(result) `mustBe` OK
+        contentAsString(result) `mustBe` view(form.fill("answer"), waypoints, countryIndex, country)(request, messages(application)).toString
+      }
+    }
+
+    "must save user answers and redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[AuthenticatedUserAnswersRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswersWithVatInfo))
+          .overrides(
+            bind[AuthenticatedUserAnswersRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, euTaxReferenceRoute)
+            .withFormUrlEncodedBody(("value", "answer"))
+
+        val result = route(application, request).value
+        val expectedAnswers: UserAnswers = emptyUserAnswersWithVatInfo
+          .set(EuTaxReferencePage(countryIndex), "answer").success.value
+
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` EuTaxReferencePage(countryIndex).navigate(waypoints, emptyUserAnswersWithVatInfo, expectedAnswers).url
+        verify(mockSessionRepository, times(1)).set(eqTo(expectedAnswers))
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, euTaxReferenceRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val view = application.injector.instanceOf[EuTaxReferenceView]
+
+        val result = route(application, request).value
+
+        status(result) `mustBe` BAD_REQUEST
+        contentAsString(result) `mustBe` view(boundForm, waypoints, countryIndex, country)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, euTaxReferenceRoute)
+
+        val result = route(application, request).value
+
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, euTaxReferenceRoute)
+            .withFormUrlEncodedBody(("value", "answer"))
+
+        val result = route(application, request).value
+
+        status(result) `mustBe` SEE_OTHER
+        redirectLocation(result).value `mustBe` routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
