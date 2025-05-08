ed4ff35b3b84325f1bfa4effd50d8068c66731bd
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-05-08 16:52:27 +0100

    VEI-144: Add Contact Details View and update the Controller.

--- app/controllers/ContactDetailsController.scala
+++ app/controllers/ContactDetailsController.scala
@@ -1,14 +1,28 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
-import controllers.actions._
+import controllers.actions.*
 import forms.ContactDetailsFormProvider
+
 import javax.inject.Inject
-import models.Mode
-import navigation.Navigator
-import pages.ContactDetailsPage
+import pages.{ContactDetailsPage, Waypoints}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.ContactDetailsView
 
@@ -16,41 +30,46 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class ContactDetailsController @Inject()(
                                       override val messagesApi: MessagesApi,
-                                      sessionRepository: SessionRepository,
-                                      navigator: Navigator,
-                                      identify: IdentifierAction,
-                                      getData: DataRetrievalAction,
-                                      requireData: DataRequiredAction,
+                                      cc: AuthenticatedControllerComponents,
                                       formProvider: ContactDetailsFormProvider,
-                                      val controllerComponents: MessagesControllerComponents,
                                       view: ContactDetailsView
                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  val form = formProvider()
+  private val form = formProvider()
+  protected val controllerComponents: MessagesControllerComponents = cc
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
-    implicit request =>
+  def onPageLoad(waypoints: Waypoints): Action[AnyContent] =
+    cc.authAndGetData() {
+      implicit request =>
 
-      val preparedForm = request.userAnswers.get(ContactDetailsPage) match {
-        case None => form
-        case Some(value) => form.fill(value)
-      }
+        val ossRegistration = request.latestOssRegistration
+        val numberOfIossRegistrations = request.numberOfIossRegistrations
 
-      Ok(view(preparedForm, mode))
-  }
+        val preparedForm = request.userAnswers.get(ContactDetailsPage) match {
+          case None => form
+          case Some(value) => form.fill(value)
+        }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
-    implicit request =>
+        Ok(view(preparedForm, waypoints, ossRegistration, numberOfIossRegistrations))
+    }
 
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+  def onSubmit(waypoints: Waypoints): Action[AnyContent] =  {
+    cc.authAndGetData().async {
+      implicit request =>
 
-        value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(ContactDetailsPage, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(ContactDetailsPage, mode, updatedAnswers))
-      )
+        val ossRegistration = request.latestOssRegistration
+        val numberOfIossRegistrations = request.numberOfIossRegistrations
+
+        form.bindFromRequest().fold(
+          formWithErrors =>
+            Future.successful(BadRequest(view(formWithErrors, waypoints, ossRegistration, numberOfIossRegistrations))),
+
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(ContactDetailsPage, value))
+              _              <- cc.sessionRepository.set(updatedAnswers)
+            } yield Redirect(ContactDetailsPage.navigate(waypoints, updatedAnswers, updatedAnswers).route)
+        )
+    }
   }
 }
