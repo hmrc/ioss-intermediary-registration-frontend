9edbe3367b1ee4f03f20ce413aa0d296f33355fd
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-07-23 12:44:05 +0100

    VEI-163: Add functionality to cancel an amendment.

--- app/controllers/amend/CancelAmendRegistrationController.scala
+++ app/controllers/amend/CancelAmendRegistrationController.scala
@@ -1,55 +1,69 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.amend
 
+import config.FrontendAppConfig
 import controllers.actions.*
 import forms.amend.CancelAmendRegistrationFormProvider
-import models.Mode
-import pages.amend.CancelAmendRegistrationPage
+import pages.Waypoints
+import pages.amend.ChangeRegistrationPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.CancelAmendRegistrationView
+import views.html.amend.CancelAmendRegistrationView
+import utils.FutureSyntax.FutureOps
 
 import javax.inject.Inject
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 class CancelAmendRegistrationController @Inject()(
-                                         override val messagesApi: MessagesApi,
-                                         sessionRepository: SessionRepository,
-                                         navigator: Navigator,
-                                         identify: IdentifierAction,
-                                         getData: DataRetrievalAction,
-                                         requireData: DataRequiredAction,
-                                         formProvider: CancelAmendRegistrationFormProvider,
-                                         val controllerComponents: MessagesControllerComponents,
-                                         view: CancelAmendRegistrationView
+                                                   override val messagesApi: MessagesApi,
+                                                   sessionRepository: SessionRepository,
+                                                   cc: AuthenticatedControllerComponents,
+                                                   formProvider: CancelAmendRegistrationFormProvider,
+                                                   val controllerComponents: MessagesControllerComponents,
+                                                   appConfig: FrontendAppConfig,
+                                                   view: CancelAmendRegistrationView
                                  )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  val form = formProvider()
+  private val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(waypoints: Waypoints): Action[AnyContent] = cc.authAndGetData(inAmend = true) {
     implicit request =>
 
-      val preparedForm = request.userAnswers.get(CancelAmendRegistrationPage) match {
-        case None => form
-        case Some(value) => form.fill(value)
-      }
-
-      Ok(view(preparedForm, mode))
+      Ok(view(form, waypoints))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(waypoints: Waypoints): Action[AnyContent] = cc.authAndGetData(inAmend = true).async {
     implicit request =>
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          BadRequest(view(formWithErrors, waypoints)).toFuture,
 
         value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(CancelAmendRegistrationPage, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(CancelAmendRegistrationPage, mode, updatedAnswers))
+          if (value) {
+            for {
+              _              <- sessionRepository.clear(request.userId)
+            } yield Redirect(appConfig.intermediaryYourAccountUrl)
+          } else {
+            Redirect(ChangeRegistrationPage.route(waypoints).url).toFuture
+          }
       )
   }
 }
--- test/controllers/amend/CancelAmendRegistrationControllerSpec.scala
+++ test/controllers/amend/CancelAmendRegistrationControllerSpec.scala
@@ -1,29 +1,46 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.amend
 
 import base.SpecBase
+import config.FrontendAppConfig
 import forms.amend.CancelAmendRegistrationFormProvider
-import models.{NormalMode, UserAnswers}
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.{never, times, verify, when}
 import org.scalatestplus.mockito.MockitoSugar
-import pages.amend.CancelAmendRegistrationPage
+import pages.{EmptyWaypoints, Waypoints}
+import pages.amend.ChangeRegistrationPage
 import play.api.inject.bind
-import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
-import repositories.SessionRepository
-import views.html.CancelAmendRegistrationView
+import repositories.AuthenticatedUserAnswersRepository
+import uk.gov.hmrc.http.SessionKeys
+import views.html.amend.CancelAmendRegistrationView
 
 import scala.concurrent.Future
 
 class CancelAmendRegistrationControllerSpec extends SpecBase with MockitoSugar {
 
-  def onwardRoute = Call("GET", "/foo")
+  private val waypoints: Waypoints = EmptyWaypoints
 
-  val formProvider = new CancelAmendRegistrationFormProvider()
-  val form = formProvider()
+  private val formProvider = new CancelAmendRegistrationFormProvider()
+  private val form = formProvider()
 
-  lazy val cancelAmendRegistrationRoute = routes.CancelAmendRegistrationController.onPageLoad(NormalMode).url
+  private lazy val cancelAmendRegistrationRoute = routes.CancelAmendRegistrationController.onPageLoad(waypoints).url
 
   "CancelAmendRegistration Controller" - {
 
@@ -39,102 +56,62 @@ class CancelAmendRegistrationControllerSpec extends SpecBase with MockitoSugar {
         val view = application.injector.instanceOf[CancelAmendRegistrationView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, waypoints)(request, messages(application)).toString
       }
     }
 
-    "must populate the view correctly on a GET when the question has previously been answered" in {
+    "must delete the amended answers and redirect to yourAccount page when the user answers Yes" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(CancelAmendRegistrationPage, true).success.value
+      val mockSessionRepository = mock[AuthenticatedUserAnswersRepository]
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
-
-      running(application) {
-        val request = FakeRequest(GET, cancelAmendRegistrationRoute)
-
-        val view = application.injector.instanceOf[CancelAmendRegistrationView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
-      }
-    }
-
-    "must redirect to the next page when valid data is submitted" in {
-
-      val mockSessionRepository = mock[SessionRepository]
-
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+      when(mockSessionRepository.clear(any())) thenReturn Future.successful(true)
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
-          .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
-          )
-          .build()
+        applicationBuilder(userAnswers = Some(basicUserAnswersWithVatInfo))
+          .overrides(bind[AuthenticatedUserAnswersRepository].toInstance(mockSessionRepository)).build()
 
       running(application) {
-        val request =
-          FakeRequest(POST, cancelAmendRegistrationRoute)
-            .withFormUrlEncodedBody(("value", "true"))
+        val sessionId = "12345-credId"
+
+        val request = FakeRequest(POST, cancelAmendRegistrationRoute)
+          .withSession(SessionKeys.sessionId -> sessionId)
+          .withFormUrlEncodedBody(("value", "true"))
+
+        val config = application.injector.instanceOf[FrontendAppConfig]
 
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+        redirectLocation(result).value mustEqual config.intermediaryYourAccountUrl
+        verify(mockSessionRepository, times(1)).clear(eqTo(sessionId))
       }
+
     }
 
-    "must return a Bad Request and errors when invalid data is submitted" in {
+    "must NOT delete the amended answers and redirect to ChangeYourRegistration page when the user answers No" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val mockSessionRepository = mock[AuthenticatedUserAnswersRepository]
 
-      running(application) {
-        val request =
-          FakeRequest(POST, cancelAmendRegistrationRoute)
-            .withFormUrlEncodedBody(("value", ""))
+      when(mockSessionRepository.clear(any())) thenReturn Future.successful(true)
 
-        val boundForm = form.bind(Map("value" -> ""))
-
-        val view = application.injector.instanceOf[CancelAmendRegistrationView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
-      }
-    }
-
-    "must redirect to Journey Recovery for a GET if no existing data is found" in {
-
-      val application = applicationBuilder(userAnswers = None).build()
+      val application =
+        applicationBuilder(userAnswers = Some(basicUserAnswersWithVatInfo))
+          .overrides(bind[AuthenticatedUserAnswersRepository].toInstance(mockSessionRepository)).build()
 
       running(application) {
-        val request = FakeRequest(GET, cancelAmendRegistrationRoute)
+        val sessionId = "12345-credId"
 
+        val request = FakeRequest(POST, cancelAmendRegistrationRoute)
+          .withSession(SessionKeys.sessionId -> sessionId)
+          .withFormUrlEncodedBody(("value", "false"))
+        
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual ChangeRegistrationPage.route(waypoints).url
+        verify(mockSessionRepository, never()).set(eqTo(basicUserAnswersWithVatInfo))
       }
-    }
 
-    "must redirect to Journey Recovery for a POST if no existing data is found" in {
-
-      val application = applicationBuilder(userAnswers = None).build()
-
-      running(application) {
-        val request =
-          FakeRequest(POST, cancelAmendRegistrationRoute)
-            .withFormUrlEncodedBody(("value", "true"))
-
-        val result = route(application, request).value
-
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
-      }
     }
   }
 }
