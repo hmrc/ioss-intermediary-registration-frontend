47141900301ddbd54420ca2ede90b73d8c9a0583
Andrew Smith <andy@andyroo.info>
2026-04-29 15:59:25 +0100

    VEOSS-2175 encrypt user answers on authenticated session

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -31,6 +31,8 @@ class FrontendAppConfig @Inject()(configuration: Configuration) {
   val appName: String = configuration.get[String]("appName")
   val origin: String = configuration.get[String]("origin")
 
+  val encryptionKey: String = configuration.get[String]("mongodb.encryption.key")
+
   private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "ioss-intermediary-registration-frontend"
 
